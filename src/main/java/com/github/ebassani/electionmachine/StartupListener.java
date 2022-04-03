7a255031842638409aaef5b9ac65fada6836d709
Martin Gaens <djsushi123@gmail.com>
2022-04-03 17:58:37 +0300

    Add a secret loader This secret loader is going to load all the secrets from the file 'secrets.txt' at startup.

---
+++ src/main/java/com/github/ebassani/electionmachine/StartupListener.java
@@ -0,0 +1,18 @@
+package com.github.ebassani.electionmachine;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class StartupListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("Context is being initialized.");
+        try {
+            Constants.load();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
