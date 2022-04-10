1364f49cf32535207971f355d0e13f13ff30bb8b
Edu <eduardo.sbassani@gmail.com>
2022-04-11 00:31:54 +0300

    Possible serialization of user

--- src/main/java/com/github/ebassani/electionmachine/Util.java
+++ src/main/java/com/github/ebassani/electionmachine/Util.java
@@ -2,12 +2,14 @@ package com.github.ebassani.electionmachine;
 
 import com.github.ebassani.electionmachine.data.Database;
 
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Base64;
 
 public class Util {
 
@@ -52,4 +54,28 @@ public class Util {
 
         return sb.toString();
     }
+//
+//    /**
+//     * Read the object from Base64 string.
+//     */
+//    public static Object fromString(String s) throws IOException,
+//            ClassNotFoundException {
+//        byte[] data = Base64.getDecoder().decode(s);
+//        ObjectInputStream ois = new ObjectInputStream(
+//                new ByteArrayInputStream(data));
+//        Object o = ois.readObject();
+//        ois.close();
+//        return o;
+//    }
+//
+//    /**
+//     * Write the object to a Base64 string.
+//     */
+//    public static String toString(Serializable o) throws IOException {
+//        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//        ObjectOutputStream oos = new ObjectOutputStream(baos);
+//        oos.writeObject(o);
+//        oos.close();
+//        return Base64.getEncoder().encodeToString(baos.toByteArray());
+//    }
 }
